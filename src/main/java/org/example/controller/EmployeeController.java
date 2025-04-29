23ca292c1a9fc2069498dc1f4492a4c0e648a5a5
shehanminura <shehanminuradp@gmail.com>
2025-04-29 10:30:09 +0530

    set up controller

---
+++ src/main/java/org/example/controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package org.example.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+@RequiredArgsConstructor
+@CrossOrigin
+
+public class EmployeeController {
+
+}
